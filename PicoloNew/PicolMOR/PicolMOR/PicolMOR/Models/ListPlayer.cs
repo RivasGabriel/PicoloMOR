3ec6596d39092b09852b7d508395509253c48c7c
RivasGabriel <90380140+RivasGabriel@users.noreply.github.com>
2022-04-26 10:04:51 +0200

    ModelPlayer

--- PicoloNew/PicolMOR/PicolMOR/PicolMOR/Models/ListPlayer.cs
+++ PicoloNew/PicolMOR/PicolMOR/PicolMOR/Models/ListPlayer.cs
@@ -16,14 +16,20 @@ namespace PicolMOR.Models
             set { tabPlayer = value; }
         }
 
-        public void RemplireList()
+        public void PlayerList()
         {
             TabPlayer.Add(new Player { Name = "Matisse", gender = Player.Gender.Homme });
             TabPlayer.Add(new Player { Name = "Gaby", gender = Player.Gender.Autre });
             TabPlayer.Add(new Player { Name = "Alex", gender = Player.Gender.Homme });
             TabPlayer.Add(new Player { Name = "Keira", gender = Player.Gender.Femme });
-            TabPlayer.Add(new Player { Name = "Veincent", gender = Player.Gender.Homme });
+            TabPlayer.Add(new Player { Name = "Vincent", gender = Player.Gender.Homme });
             TabPlayer.Add(new Player { Name = "Kevin", gender = Player.Gender.Homme });
+        }
+
+        public void RemplirList(String Name, Player.Gender genre)
+        {
+            genre = Player.Gender.Autre; // defaut
+            TabPlayer.Add(new Player { Name = Name, gender = genre });
         }
     }
 }
